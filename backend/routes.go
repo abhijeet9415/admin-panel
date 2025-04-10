6675e67fdd8d7f3dfc38b5793064f9bd54602b2b
abhijeet9415 <abhijeetsinghspn@gmail.com>
2025-04-10 14:43:54 +0530

    route issue

--- backend/routes.go
+++ backend/routes.go
@@ -10,7 +10,7 @@ func SetupRoutes(r *mux.Router) {
 	r.HandleFunc("/users/register", RegisterUser).Methods("POST")
 	r.HandleFunc("/users/login", LoginUser).Methods("POST")
 	r.HandleFunc("/users/update-password", UpdatePassword).Methods("POST")
-	r.HandleFunc("/GetUsers", GetAllUsers).Methods("GET")
+	r.HandleFunc("/users", GetAllUsers).Methods("GET")
 	r.Handle("/users/{id}", AuthMiddleware(http.HandlerFunc(GetUser))).Methods("GET")
 
 }
